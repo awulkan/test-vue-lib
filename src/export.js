86f898b1d744400c45343e7e0d3cf5ff8120fbf9
Alexander <alexanderwulkan@hotmail.com>
2021-01-13 11:05:48 +0100

    Change: Export names

--- src/export.js
+++ src/export.js
@@ -1,7 +1,4 @@
 import VButton from './components/buttons/VButton.vue'
-import VSpinner from './components/progress/VProgressCircular.vue'
+import VProgressCircular from './components/progress/VProgressCircular.vue'
 
-export default {
-  VButton,
-  VSpinner,
-}
+export { VButton, VProgressCircular }
